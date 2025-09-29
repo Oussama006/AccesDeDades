914a16c9bfd69952398e17adad5c0a074275905a
OussamaAH6 <oussama.allali@inskta.cat>
2025-09-29 19:08:13 +0200

    lliurament_UserEx41_29_09_2025

---
+++ UserEx41.java
@@ -0,0 +1,11 @@
+public class UserEx41 implements java.io.Serializable{
+
+    String nom;
+    String password;
+    
+    public UserEx41(String oussama, String passw){
+
+        this.nom = oussama;
+        this.password = passw;
+    }
+}
