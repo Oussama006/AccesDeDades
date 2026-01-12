506421d96e1cc7aca2d9218f29bb025d3bf841fe
OussamaAH6 <oussama.allali@inskta.cat>
2026-01-12 18:23:12 +0100

    Final

---
+++ MongoDB/mongodb/src/main/java/cat/dam/Main.java
@@ -0,0 +1,58 @@
+package cat.dam;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import java.util.Date;
+import org.bson.Document;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // PON AQUÍ TU CONTRASEÑA REAL
+        String uri = "mongodb+srv://oussamaallali_db_user:MiPass123@cluster0.gf6hsvq.mongodb.net/CotxesEsportius?retryWrites=true&w=majority";
+
+        // Crear cliente y conectar
+        MongoClient client = MongoClients.create(uri);
+        MongoDatabase db = client.getDatabase("CotxesEsportius");
+        MongoCollection<Document> cotxes = db.getCollection("cotxes");
+
+        System.out.println("Connexió correcta a MongoDB Atlas");
+
+        // Insertar un cotxe
+        Document cotxe = new Document()
+                .append("marca", "Audi")
+                .append("model", "RS6 Java")
+                .append("potenciaCV", 600)
+                .append("preu", 140000)
+                .append("dataAlta", new Date())
+                .append("disponible", true)
+                .append("descripcio", "Cotxe inserit des de Java");
+
+        cotxes.insertOne(cotxe);
+        System.out.println("Cotxe inserit");
+
+        // Listar cotxes
+        System.out.println("Llistat de cotxes:");
+        for (Document d : cotxes.find()) {
+            System.out.println(d.toJson());
+        }
+
+        // Actualizar cotxe
+        cotxes.updateOne(
+                eq("model", "RS6 Java"),
+                new Document("$set", new Document("disponible", false))
+        );
+        System.out.println("Cotxe actualitzat");
+
+        // Eliminar cotxe
+        cotxes.deleteOne(eq("model", "RS6 Java"));
+        System.out.println("Cotxe eliminat");
+
+        // Cerrar conexión
+        client.close();
+    }
+}
